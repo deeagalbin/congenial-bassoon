88006072daa7e8454cb8aaae1aa70e96c9ea9069
deeagalbin <48330267+deeagalbin@users.noreply.github.com>
2020-05-09 21:48:33 +0300

    Update functie_evaluare_conexitate.py

--- functie_evaluare_conexitate.py
+++ functie_evaluare_conexitate.py
@@ -4,6 +4,9 @@ noduri_totale = len(data)
 
 
 def conex(comunitate):
+    #verificam daca intr-o comunitate ramasa avem intr-adevar o comunitate odata cu scoaterea unui nod
+    #comunitate=comunitatea ramasa dupa ce scoatem un nod
+    #parcurgem comunitatea si utilizam un vector vizitat initializat cu 0,daca avem vreun nod care nu a fost vizitata macar o data,inseamna ca acesta este izolat
     gasit = 1
     n = len(comunitate)
     vizitat = [0 for i in range(noduri_totale)]
@@ -15,6 +18,9 @@ def conex(comunitate):
 
 
 def verifica_legaturi(comunitate, vizitat):
+    #comunitate=comunitatea pt care verificam legaturiile
+    #vizitat=vectorul de zerouri initializat
+    #parcurgem bfs o comunitate si cu ajutorul matricii de adicente prelucram vectorul vizitat care ne va spune daca nodul a fost sau nu parcurs 
     for i in range(0, len(comunitate)):
         for nod in comunitate:
             if matrix[comunitate[i] - 1][nod - 1] == 1 and vizitat[nod - 1] == 0:
