cad0808257a3154b42363d7bad01e31ddead67a1
deeagalbin <48330267+deeagalbin@users.noreply.github.com>
2020-05-11 09:33:01 +0300

    Update solutie_initiala.py

--- solutie_initiala.py
+++ solutie_initiala.py
@@ -2,7 +2,9 @@ from parsare_fisier import *
 
 visited = []
 
-
+#root_graf-nodul de la care pornim
+    #graf-comunitati-graful initial
+    #visited-o lista in care punem toate noduriile deja aflate in comunitate pentru a nu ne repeta
 def find_community(root_graf, graf_comunitati, visited):
     community = [root_graf]
     visited.append(root_graf)
@@ -14,6 +16,10 @@ def find_community(root_graf, graf_comunitati, visited):
     return community
 
 
+#graf_comunitati=graful construit in citirea datelor care este o lista de liste avand ca si indice nodul iar ca elemente in lista toate noduriile de care acesta se leaga
+    #data=noduriile noastre
+    #cosntruim inceand de la primul nod din comunitate un grup de noduri in mici comunitati
+    #returneaza solutia initiala 
 def communities_graph(graf_comunitati, data):
     communities = []
     for nod in data:
